f572924a88b7dbde409548ca620c2b7122715b80
skamuelgo <samuel@cmbs-g.com>
2023-05-23 03:05:42 -0500

    updated the about combar page

--- hub/src/About.tsx
+++ hub/src/About.tsx
@@ -1,5 +1,15 @@
+import { AboutHeading, LogoContainer, LogoPic, AboutCombar } from "./styles";
+
 export const About = () => (
   <div>
-    <h2>About page</h2>
+    <LogoContainer>
+      <LogoPic />
+    </LogoContainer>
+    <AboutHeading>Unlocking Insights: Connect, Inquire, Empower.</AboutHeading>
+    <AboutCombar>
+      combar: a universal comment bar on every webpage. <br />
+      It’s a chrome extension that allows users to select text  <br />
+      and create comments in the margins of webpages.
+    </AboutCombar>
   </div>
 );
--- hub/src/styles/index.ts
+++ hub/src/styles/index.ts
@@ -62,3 +62,33 @@ export const CommentText = styled.p`
 export const CenteredHeading = styled.h1`
   text-align: center;
 `;
+export const AboutHeading = styled.h1`
+  text-align: center;
+  margin-top: 20px; /* Adjust the value as needed */
+`;
+export const AboutCombar = styled.h3`
+  text-align: center;
+  margin-top: 20px; /* Adjust the value as needed */
+  font-family: Arial, sans-serif; /* Change the font-family to your desired font */
+  color: #555; /* Adjust the color to your preferred shade */
+`;
+export const LogoContainer = styled.div`
+  margin-top: 200px; /* Adjust the value as needed */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh; /* Adjust the height as needed */
+`;
+export const LogoPic = styled.div`
+  width: 5em; /* Increase the value to make it wider */
+  height: 5em; /* Increase the value to make it taller */
+  border-radius: 50%;
+  background-image: url(./pracLogo.jpg);
+  background-size: cover;
+  background-position: center;
+  background-color: #ccc;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
